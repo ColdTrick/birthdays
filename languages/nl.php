5390b629625017fbf409ecaaeb6312af13f0fe0d
Jerome Bakker <jeabakker@coldtrick.com>
2012-07-26 16:48:01 +0200

    added: dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,24 @@
+<?php
+$language = array (
+  'birthdays:no_field_configured' => 'Er is nog geen verjaardagsveld geconfigureerd, vraag je site beheerder om de plugin te configureren',
+  'birthdays:more' => 'Meer verjaardagen',
+  'birthdays:none' => 'Geen verjaardagen gevonden',
+  'birthdays:menu:site:title' => 'Verjaardagen',
+  'birthdays:filter:upcoming' => 'Binnenkort jarig',
+  'birthdays:menu:owner_block:group:title' => 'Groepsverjaardagen',
+  'birthdays:breadcrumb:all' => 'Verjaardagen',
+  'brithdays:profile_field:type' => 'Verjaardag',
+  'birthdays:settings:field_selection:description' => 'Selecteer het profiel veld welke de verjaardag bevat van je gebruikers.',
+  'birthdays:settings:field_selection:none' => 'Weergeven van verjaardagen uitschakelen',
+  'birthdays:settings:output_format:description' => 'Hier kun je het formaat van de verjaardag velden aanpassen',
+  'birthdays:page:day:title' => 'Verjaardagen op: %s',
+  'birthdays:page:friends:title' => 'Verjaardagen van %s\'s vrienden',
+  'birthdays:page:upcoming:title' => 'Binnenkort jarig',
+  'birthdays:page:group:title' => 'Verjaardagen van %s\'s leden',
+  'birthdays:sidebar:datepicker:title' => 'Toon verjaardagen op',
+  'birtdays:widget:title' => 'Verjaardagen',
+  'birtdays:widget:description' => 'Toon de verjaardagen van iedereen, je vrienden of van groepsleden',
+  'birthdays:groups:options' => 'Toon de verjaardagen van de groepsleden',
+  'birthdays:groups:not_enabled' => 'Weergave van verjaardagen is niet ingeschakeld voor deze groep',
+);
+add_translation("nl", $language);
